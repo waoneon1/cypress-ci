232f8c9365a48d022beb97be7ed0bf278810f366
masagussetyop <agus@alterra.id>
2021-03-16 14:35:23 +0700

    [HOTO-8] change

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,34 @@
 module.exports = {
-	extends: ['airbnb-typescript'],
-	parserOptions: {
-		project: './tsconfig.json',
-	}
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['plugin:vue/essential', 'airbnb-base', 'plugin:jest/recommended'],
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['vue', 'jest'],
+  globals: {
+    $nuxt: true,
+  },
+  rules: {
+    'max-len': 0,
+    'no-underscore-dangle': ['error', { allow: ['_chart'] }],
+  },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './'],
+          ['~', './'],
+        ],
+        extensions: ['.ts', '.js', '.jsx', '.json'],
+      },
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json', '.vue'],
+      },
+    },
+  },
 };
