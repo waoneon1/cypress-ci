fe319ee4958d15ca5708fa90a33e9ef5b698b964
masagussetyop <agus@alterra.id>
2021-03-18 16:33:26 +0700

    [HOTI-12] posision change

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="bg-gray-100 overflow-x-hidden">
+  <div class="bg-gray-100 h-screen overflow-x-hidden">
     <Alert v-show="alert" :title="alertTitle" :description="alertDescription" :theme="alertTheme" />
-    <div class="my-0 mx-auto h-full max-w-md p-10 bg-white">
-      <div class="flex items-center mb-10 ">
+    <div class="relative my-0 mx-auto h-full max-w-md bg-white p-5">
+      <div class="flex items-center mb-5 ">
         <svg width="44" height="44" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M28.6732 13.6405C28.2018 14.1 27.9868 14.7619 28.098 15.4107L28.6328 18.5285C28.913 20.1623 27.1982 21.4082 25.7309 20.6368L22.9307 19.1647C22.348 18.8584 21.652 18.8584 21.0693 19.1647L18.2691 20.6369C16.8018 21.4082 15.087 20.1623 15.3672 18.5285L15.9019 15.4107C16.0132 14.7619 15.7982 14.1 15.3268 13.6405L13.0609 11.4315C11.874 10.2744 12.5291 8.25855 14.1695 8.02021L17.3005 7.56528C17.9519 7.47063 18.5151 7.06148 18.8064 6.47117L20.2065 3.6341C20.9401 2.14758 23.0599 2.14758 23.7935 3.6341L25.1936 6.47117C25.4849 7.06148 26.0481 7.47063 26.6995 7.56528L29.8305 8.02021C31.4709 8.25855 32.126 10.2744 30.9391 11.4315L28.6732 13.6405Z" fill="#FFA91E"/>
           <path d="M32.4849 22.8828C33.7989 22.3557 35.2296 23.3232 35.2296 24.739V27.9698C35.2296 28.7837 34.7363 29.5165 33.9823 29.8228L22.1545 34.6275C22.105 34.6476 22.0496 34.6478 22 34.6279L10.0258 29.824C9.26745 29.5198 8.77045 28.7849 8.77045 27.9678V24.739C8.77045 23.3232 10.2011 22.3557 11.5151 22.8828L21.2553 26.7904C21.7333 26.9821 22.2667 26.9821 22.7447 26.7904L32.4849 22.8828Z" fill="#FFA91E"/>
@@ -16,11 +16,11 @@
           <p class="text-xs">Relative Ranking System</p>
         </div>
       </div>
-      <div class="font-mulish mb-16">
-        <h1 class="text-4xl font-black leading-tight text-primary mb-4">Know Your Best Be The Best</h1>
+      <div class="font-roboto mb-16">
+        <h1 class="text-4xl font-black leading-tight text-primary mb-4">Know Your Best<br> Be The Best</h1>
         <p class="text-sm font-light text-primary">Learn from The Best</p>
       </div>
-      <div class="flex justify-center">
+      <!-- <div class="flex justify-center">
         <div class="relative">
           <img class="absolute top-0 mt-8" src="~/static/img/login_achive.png" style="left: -60px;"/>
           <div class="rounded-3xl shadow-lg p-10 inline-block">
@@ -29,22 +29,24 @@
           </div>
           <img class="absolute top-0 mt-16" src="~/static/img/login_grap.png" style="right: -60px;"/>
         </div>
-      </div>
-      <div class="mt-16">
-        <div class="mr-4 md:mr-0 rounded-full py-4 px-4 sm:py-2 sm:px-8 border border-solid border-secondary bg-secondary text-white focus:outline-none cursor-pointer"
-          @click="handleClickLogin"
-        >
-          <div class="flex items-center mx-auto justify-center">
-            <svg class="mr-3" width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M22 11.8969C22 18.5297 17.6316 23.25 11.1803 23.25C4.99508 23.25 0 18.0562 0 11.625C0 5.19375 4.99508 0 11.1803 0C14.1918 0 16.7254 1.14844 18.6775 3.04219L15.6344 6.08438C11.6537 2.09063 4.25123 5.09063 4.25123 11.625C4.25123 15.6797 7.36639 18.9656 11.1803 18.9656C15.6074 18.9656 17.2664 15.6656 17.5279 13.9547H11.1803V9.95625H21.8242C21.9279 10.5516 22 11.1234 22 11.8969Z" fill="white"/>
-            </svg>
-            <span class="font-bold">Sign in with Gmail</span>
+      </div> -->
+      <div class="bottom-0 absolute w-full">
+        <div class="relative">
+          <div class="md:mr-0 rounded-full py-4 px-4 sm:py-2 sm:px-8 border border-solid border-secondary bg-secondary text-white focus:outline-none cursor-pointer"
+            @click="handleClickLogin"
+          >
+            <div class="flex items-center mx-auto justify-center">
+              <svg class="mr-3" width="22" height="24" viewBox="0 0 22 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M22 11.8969C22 18.5297 17.6316 23.25 11.1803 23.25C4.99508 23.25 0 18.0562 0 11.625C0 5.19375 4.99508 0 11.1803 0C14.1918 0 16.7254 1.14844 18.6775 3.04219L15.6344 6.08438C11.6537 2.09063 4.25123 5.09063 4.25123 11.625C4.25123 15.6797 7.36639 18.9656 11.1803 18.9656C15.6074 18.9656 17.2664 15.6656 17.5279 13.9547H11.1803V9.95625H21.8242C21.9279 10.5516 22 11.1234 22 11.8969Z" fill="white"/>
+              </svg>
+              <span class="font-bold">Sign in with Gmail</span>
+            </div>
           </div>
+          <p class="flex items-center mx-auto justify-center mt-3 mb-3">
+            <span class="text-sm font-light text-primary pt-2 mr-2">Product by</span>
+            <img src="~/static/img/logo_alterra.png" alt="alterra rrs" style="width:57px; height:20px;"/>
+          </p>
         </div>
-        <p class="flex items-center mx-auto justify-center mt-6 ">
-          <span class="text-sm font-light text-primary pt-2 mr-2">Product by</span>
-          <img src="~/static/img/logo_alterra.png" alt="alterra rrs" style="width:57px; height:20px;"/>
-        </p>
       </div>
     </div>
   </div>
