5d3c2f3c1aaa132162a538f821c75a30e18eb7f2
masagussetyop <agus@alterra.id>
2021-03-22 14:06:37 +0700

    [HOTI-12] by data object

--- components/ComponentsRandomPict.vue
+++ components/ComponentsRandomPict.vue
@@ -8,13 +8,11 @@
           style="left: -60px;"
         />
         <div class="rounded-3xl shadow-lg md:p-12 p-6 inline-block">
-          <div class="relative">
-            <img src="~/static/img/img-01.png" width="75px" class="ml-16"/>
-            <img src="~/static/img/img-02.png" width="50px" class="ml-1 -mt-5"/>
-            <img src="~/static/img/img-03.png" width="40px" class="ml-32 -mt-8"/>
-            <img src="~/static/img/img-04.png" width="35px" class="ml-12 -mt-3"/>
-            <img src="~/static/img/img-05.png" width="35px" class="ml-24 -mt-4"/>
-            <img src="~/static/img/img-06.png" width="15px" class="ml-20"/>
+          <div 
+            v-for="(empl, index) in emplList"
+            :key="index"
+          >
+            <img :src="empl.src" :width="empl.width" :class="empl.class" />
           </div>
           <p class="font-medium text-sm text-primary w-full text-center">
             Relative Ranking System
@@ -31,11 +29,50 @@
 </template>
 
 <script lang="ts">
-import Vue from "vue";
+import { Vue, Component, Prop } from 'vue-property-decorator'
 
-export default Vue.extend({
-  
-});
+interface emplType {
+  src: string,
+  width: string,
+  class: string
+}
+
+@Component
+export default class ComponentsRandomPict extends Vue {
+  emplList: emplType[] =
+  [
+    {
+      src: '/img/img-01.png',
+      width: '75px',
+      class: 'ml-16'
+    },
+    {
+      src: '/img/img-02.png',
+      width: '50px',
+      class: 'ml-1 -mt-5'
+    },
+    {
+      src: '/img/img-03.png',
+      width: '40px',
+      class: 'ml-32 -mt-8'
+    },
+    {
+      src: '/img/img-04.png',
+      width: '35px',
+      class: 'ml-12 -mt-3'
+    },
+    {
+      src: '/img/img-05.png',
+      width: '35px',
+      class: 'ml-24 -mt-4'
+    },
+    {
+      src: '/img/img-06.png',
+      width: '15px',
+      class: 'ml-20'
+    },
+  ]
+}
 </script>
 
 <style scoped></style>
