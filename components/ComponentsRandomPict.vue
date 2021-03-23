3765b0a7ff879d174bebf68d461cf9088b55e0f8
masagussetyop <agus@alterra.id>
2021-03-23 11:44:17 +0700

    [HOTI-12] clear and add JEST testing

--- components/ComponentsRandomPict.vue
+++ components/ComponentsRandomPict.vue
@@ -75,13 +75,10 @@ export default class ComponentsRandomPict extends Vue {
     let temporaryValue;
     let randomIndex;
 
-    // While there remain elements to shuffle...
     while (currentIndex !== 0) {
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
 
-      // And swap it with the current element.
       temporaryValue = dataRdm[currentIndex];
       dataRdm[currentIndex] = dataRdm[randomIndex];
       dataRdm[randomIndex] = temporaryValue;
