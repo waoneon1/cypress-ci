da80b9629a24039407a0c2c4e3f0fde26b338165
masagussetyop <agus@alterra.id>
2021-03-25 17:29:37 +0700

    [HOTI-85] change interface

--- layouts/error.vue
+++ layouts/error.vue
@@ -9,12 +9,17 @@ import { Vue, Component, Prop } from 'vue-property-decorator';
 import error404 from '~/components/error/404.vue';
 import error500 from '~/components/error/500.vue';
 
+interface errorType {
+  statusCode: number;
+}
+
 @Component({
   name: 'nuxtError',
   layout: 'error',
 })
+
 export default class LayoutError extends Vue {
-  @Prop({ type: Object, default: () => {} }) readonly error!: any
+  @Prop({ type: Object, default: () => {} }) readonly error!: errorType
 
   // statusCode: string = '';
 
