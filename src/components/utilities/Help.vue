8a20da074aa07813e20b9e790c3c414d10f64699
masagussetyop <agus@alterra.id>
2021-05-06 13:02:47 +0700

    [HOTI-437] update data and clear img

--- src/components/utilities/Help.vue
+++ src/components/utilities/Help.vue
@@ -1,6 +1,7 @@
 <template>
   <div
-    class="absolute top-0 left-0 w-full flex bg-primary py-4 bg-opacity-90" style="margin-top:60px; height: calc(100vh - 60px);"
+    class="absolute top-0 left-0 w-full flex bg-primary py-4 bg-opacity-90"
+    style="margin-top:60px; height: calc(100vh - 60px);"
     v-show="show"
   >
     <div class="flex flex-col text-left px-5 w-full overflow-auto">
@@ -12,26 +13,14 @@
       </div>
       <div class="text-white font-base text-sm">
         <h1 class="text-white mb-5 font-bold">Aturan</h1>
-        <ol>
+        <ol v-for="(item, i) in rules" :key="i">
           <li class="mb-4">
-            Pengguna dapat memilih max. 3 alterrans per halaman yang dirasa
-            sesuai dan paling baik dengan domain dan jika menurut
-            pengguna dalam 1 halaman tidak ada yang sesuai bisa langsung
-            klik tombol lanjut untuk melanjutkan ke halaman berikutnya.
-            <img src="~/static/img/exp-selected.png" alt="exp-selected" class="rounded-lg mt-4">
-          </li>
-          <li class="mb-4">
-            Dalam pemilihan tidak tergantung dalam senior/junior jadi dalam
-            alterrans yang dipilih adalah hasil dari opini menurut mengguna yang
-            paling berkopenten dalam domain.
-          </li>
-          <li class="mb-4">
-            Progress bar menandakan status dari progress uda sampai berapa
-            persen, progress bar naik ketika dalam halaman ada yang dipilih dan
-            jika progress bar sudah mencapai angka prosentase yang ditentukan
-            pengguna dapat mengakhiri dan bisa di lanjut di lain waktu untuk
-            pemilih alterrans berdasarkan domain.
-            <img src="~/static/img/exp-progressbar.png" alt="exp-progressbar" class="rounded-lg mt-4">
+            {{ item.desc }}
+            <img
+              :src="'/img/' + item.image"
+              alt="exp-selected"
+              class="rounded-lg mt-4"
+            />
           </li>
         </ol>
       </div>
@@ -47,5 +36,26 @@ export default class Help extends Vue {
   @Prop({ required: false, type: String }) title?: string;
 
   @Prop({ required: false, type: Boolean }) show!: boolean;
+
+  rules: object[] = [
+    {
+      id: 1,
+      desc:
+        'Pengguna dapat memilih max. 3 alterrans per halaman yang dirasa sesuai dan paling baik dengan domain dan jika menurut pengguna dalam 1 halaman tidak ada yang sesuai bisa langsungklik tombol lanjut untuk melanjutkan ke halaman berikutnya.',
+      image: 'exp-selected.png',
+    },
+    {
+      id: 2,
+      desc:
+        'Dalam pemilihan tidak tergantung dalam senior/junior jadi dalam alterrans yang dipilih adalah hasil dari opini menurut mengguna yang paling berkopenten dalam domain.',
+      image: 'exp-senior.png',
+    },
+    {
+      id: 3,
+      desc:
+        'Progress bar menandakan status dari progress uda sampai berapa persen, progress bar naik ketika dalam halaman ada yang dipilih dan jika progress bar sudah mencapai angka prosentase yang ditentukan pengguna dapat mengakhiri dan bisa di lanjut di lain waktu untuk pemilih alterrans berdasarkan domain.',
+      image: 'exp-progressbar.png',
+    },
+  ];
 }
 </script>
