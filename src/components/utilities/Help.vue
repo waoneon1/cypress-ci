8edf284ab31a19c48837b9f058d266ab91411038
Ekky Patria <ekkypatria@gmail.com>
2021-05-07 15:35:18 +0700

    [updated] for domain design, construction

--- src/components/utilities/Help.vue
+++ src/components/utilities/Help.vue
@@ -5,29 +5,34 @@
     v-show="show"
   >
     <div class="flex flex-col text-left px-5 w-full overflow-auto">
-      <!-- <h2 class="text-white mb-5 font-bold">{{ title }}</h2>
-      <div class="text-white font-base text-sm">
-        <slot name="articles"></slot>
-      </div> -->
-      <div class="text-white font-base text-sm">
-        <!-- <h1 class="text-white mb-5 font-bold">Aturan</h1> -->
-        <ol v-for="(item, i) in rules" :key="i">
-          <li class="mb-4">
-            {{ item.desc }}
-            <img
-              :src="'/img/' + item.image"
-              alt="exp"
-              class="rounded-lg mt-4"
-            />
-          </li>
-        </ol>
+      <h1 class="text-white mb-5 font-bold text-lg">Competency {{ title }}</h1>
+      <div v-if="!qnaHelp">
+        <h2 class="text-white mb-5 font-bold">
+          Deskripsi
+        </h2>
+        <slot name="help-detail"></slot>
+      </div>
+      <div v-if="qnaHelp">
+        <div class="text-white font-base text-sm">
+          <h2 class="text-white mb-5 font-bold">Aturan</h2>
+          <ol v-for="(item, i) in rules" :key="i">
+            <li class="mb-4">
+              {{ item.desc }}
+              <img
+                :src="'/img/' + item.image"
+                alt="exp"
+                class="rounded-lg mt-4"
+              />
+            </li>
+          </ol>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Vue, Prop, Component } from 'vue-property-decorator';
+import { Vue, Prop, Component } from "vue-property-decorator";
 
 @Component
 export default class Help extends Vue {
@@ -35,31 +40,33 @@ export default class Help extends Vue {
 
   @Prop({ required: false, type: Boolean }) show!: boolean;
 
+  @Prop({ required: false, type: Boolean }) qnaHelp!: boolean;
+
   rules: object[] = [
     {
       id: 1,
       desc:
-        'Pengguna dapat memilih max. 3 alterrans per halaman yang akan direkomendasikan',
-      image: 'exp-selected.png',
+        "Pengguna dapat memilih max. 3 alterrans per halaman yang akan direkomendasikan",
+      image: "exp-selected.png"
     },
     {
       id: 2,
       desc:
         'Jika dalam 1 halaman tidak ada Alterrans yang bisa kamu rekomendasikan, kamu bisa langsung klik tombol "Lanjut"',
-      image: 'exp-btn-next.png',
+      image: "exp-btn-next.png"
     },
     {
       id: 3,
       desc:
-        'Dalam pemilihan tidak tergantung dalam senior/junior jadi dalam alterrans yang dipilih adalah hasil dari opini menurut mengguna yang paling berkopenten dalam domain.',
-      image: 'exp-senior.png',
+        "Dalam pemilihan tidak tergantung dalam senior/junior jadi dalam alterrans yang dipilih adalah hasil dari opini menurut mengguna yang paling berkopenten dalam domain.",
+      image: "exp-senior.png"
     },
     {
       id: 4,
       desc:
-        'Progress bar menandakan status dari progress uda sampai berapa persen, progress bar naik ketika dalam halaman ada yang dipilih dan jika progress bar sudah mencapai angka prosentase yang ditentukan pengguna dapat mengakhiri dan bisa di lanjut di lain waktu untuk pemilih alterrans berdasarkan domain.',
-      image: 'exp-progressbar.png',
-    },
+        "Progress bar menandakan status dari progress uda sampai berapa persen, progress bar naik ketika dalam halaman ada yang dipilih dan jika progress bar sudah mencapai angka prosentase yang ditentukan pengguna dapat mengakhiri dan bisa di lanjut di lain waktu untuk pemilih alterrans berdasarkan domain.",
+      image: "exp-progressbar.png"
+    }
   ];
 }
 </script>
--- src/pages/criteria/_domain.vue
+++ src/pages/criteria/_domain.vue
@@ -36,11 +36,17 @@
         <h1
           slot="title"
           class="text-sm text-white mb-10 px-5"
-          v-html="dataCriteria()"
+          v-html="dataCriteria().shortdec"
         ></h1>
       </Thankyou>
 
-      <Help :title="domain" :show="help"></Help>
+      <Help :title="domain" :show="help">
+        <div
+          slot="help-detail"
+          class="text-white text-sm"
+          v-html="dataCriteria().description"
+        ></div>
+      </Help>
     </div>
   </div>
 </template>
@@ -70,71 +76,87 @@ export default class Criteria extends Vue {
     const dataCriteria = [
       {
         criteria_name: 'Design',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk membuat <b class='text-secondary'>design arsitektur</b>, ataupun <b class='text-secondary'>design interface</b>",
+        description:
+          "<p class='mb-3'>Keterampilan desain perangkat lunak digunakan untuk mengembangkan dan menggambarkan arsitektur perangkat lunak suatu sistem berdasarkan persyaratan perangkat lunaknya: bagaimana perangkat lunak didekomposisi menjadi komponen dan antarmuka antara komponen tersebut. Komponen tersebut ditentukan pada tingkat detail yang memungkinkan konstruksinya.</p><p class='mb-3'>Bidang keterampilan ini juga mencakup keterampilan yang berkaitandengan proses dan teknik untuk kualitas desain perangkat lunak, analisis, dan evaluasi.</p>",
       },
       {
         criteria_name: 'Requirements',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk melakukan <b class='text-secondary'>proses analisis</b> baik fungsional maupun non-fungsional, <b class='text-secondary'>mengumpulkan data</b>, hingga <b class='text-secondary'>merumuskan kebutuhan</b> suatu pengembangan aplikasi.",
+        description: '',
       },
       {
         criteria_name: 'Construction',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengubah spesifikasi desain menjadi solusi perangkat lunak</b> fungsional yang memenuhi kebutuhan pelanggan. Kompetensi ini termasuk proses <b class='text-secondary'>perencanaan</b>, <b class='text-secondary'>perancangan</b>, <b class='text-secondary'>pemrograman</b>, <b class='text-secondary'>debugging</b>, <b class='text-secondary'>pengujian</b>, dan <b class='text-secondary'>pengintegrasian</b> komponen perangkat lunak.",
+        description:
+          "<p class='mb-3'>Konstruksi perangkat lunak adalah kumpulan aktivitas dan proses untuk mengubah spesifikasi desain menjadi solusi perangkat lunak fungsional yang memenuhi kebutuhan pelanggan. Domain ini termasuk perencanaan, perancangan, pemrograman, debugging, pengujian, dan pengintegrasian komponen perangkat lunak.</p><p class='mb-3'>Sebagian besar konstruksi perangkat lunak dilakukan oleh tim profesional menggunakan alat (editor, kompiler, perangkat lunak kontrol versi, debugger, dan sebagainya) dan proses untuk menyelesaikan dan mengoordinasikan pekerjaan mereka.</p>",
       },
       {
         criteria_name: 'Testing',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengevaluasi kualitas produk</b> secara keseluruhan, yang <b class='text-secondary'>memerlukan eksekusi kode</b>. Termasuk juga kemampuan melakukan verifikasi dinamis dan kemampuan dalam memilih serangkaian kasus uji yang sesuai.",
+        description: '',
       },
       {
         criteria_name: 'Sustainment',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>menangani masalah pemeliharaan</b> termasuk di dalamnya berupa dokumentasi, penerapan, operasi, keamanan, manajemen konfigurasi, pelatihan (pengguna dan personel pemeliharaan), meja bantuan, manajemen produk COTS, pembaruan teknologi, dan penghentian perangkat lunak. ",
+        description: '',
       },
       {
         criteria_name: 'Process',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengidentifikasi aktivitas proses</b> dan kombinasi yang perlu dimodifikasi untuk mencapai tujuan proyek dengan lebih baik.",
+        description: '',
       },
       {
         criteria_name: 'Systems Engineering',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengidentifikasi kebutuhan proses development</b>, membuat <b class='text-secondary'>Life Cycle Model</b>, memahami <b class='text-secondary'>konsep operasional</b> seperti limitasi, environment, depedency, serta kemampuan untuk <b class='text-secondary'>memberikan solusi-solusi alternatif</b> dengan baik.",
+        description: '',
       },
       {
         criteria_name: 'Quality',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>menghasilkan produk berkualitas tinggi</b>, yang didefinisikan sebagai produk yang sesuai dengan persyaratannya dan memenuhi kebutuhan pengguna. ",
+        description: '',
       },
       {
         criteria_name: 'Security',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengidentifikasi potensi kerentanan keamanan</b>, mereplika proses penyerang, mereview dan <b class='text-secondary'>mengetahui attack patterns</b>. Termasuk juga proses <b class='text-secondary'>collecting</b> dan <b class='text-secondary'>monitoring metrics</b> untuk memastikan bahwa proses pengembangan software sudah mengikuti prosedur yang benar.",
+        description: '',
       },
       {
         criteria_name: 'Config Management',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk melakukan <b class='text-secondary'>manajemen konfigurasi</b>, mulai dari identifikasi software configuration identification, software configuration control, software configuration status accounting, software configuration auditing, dan software release management and delivery",
+        description: '',
       },
       {
         criteria_name: 'Measurement',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk <b class='text-secondary'>mengukur atribut manajemen</b>, seperti penjadwalan dan anggaran terhadap pengembangan suatu produk. Penekanan pada kompetensi ini adalah bagaimana <b class='text-secondary'>mengukur proses</b> dan <b class='text-secondary'>produk</b> itu sendiri",
+        description: '',
       },
       {
         criteria_name: 'HCI',
-        description:
+        shortdec:
           "Alterrans yang akan kamu pilih setelah ini mempunyai kompetensi untuk memberikan Human-Computer Interaction yang baik sehingga produk dapat digunakan dengan sesuai oleh penggunanya. Proses ini terdiri mulai dari proses <b class='text-secondary'>requirements gathering</b>, <b class='text-secondary'>interaction</b> pada aplikasi, hingga melakukan <b class='text-secondary'>evaluasi</b> setelah produk diluncurkan",
+        description: '',
       },
     ];
     if (this.local) {
       const domain = JSON.parse(this.local);
-      const crit = dataCriteria.filter(
+      const domailFiltered = dataCriteria.filter(
         (arr) => arr.criteria_name === domain.criteria_name,
       )[0];
-      return crit ? crit.description : 'no data';
+      // return crit ? crit.description : 'no data';
+      return domailFiltered || 'no data';
+      // return domailFiltered ? domailFiltered : 'no data';
     }
     return 'no data';
   }
