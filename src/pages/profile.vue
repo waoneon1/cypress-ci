09c65070727a441a60577e5c896e1de76e974337
Dharmawan Sukma <waone.on1@gmail.com>
2021-05-07 16:10:34 +0700

    HOTI-489 lint

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -100,12 +100,7 @@
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
 
-  @Component
-  export default class  extends Vue {
-      
-  }
 </script>
 
 <style scoped>
