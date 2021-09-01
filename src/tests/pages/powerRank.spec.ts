378f92a9065717f4778e616125457482e2d34d0e
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-01 17:54:31 +0700

    HOTI-1279 unit testing

--- src/tests/pages/powerRank.spec.ts
+++ src/tests/pages/powerRank.spec.ts
@@ -1,10 +1,22 @@
 import PowerRank from '@/pages/powerRank.vue';
 import { expect, it, describe } from '@jest/globals';
-import { shallowMount } from '@vue/test-utils';
+import { createLocalVue, shallowMount, Wrapper } from '@vue/test-utils';
+import Vuex from 'vuex';
+import axios, { AxiosResponse } from 'axios';
+
+let wrapper: Wrapper<any>;
+
+const Vue = createLocalVue();
+Vue.use(Vuex);
+
+// Mock axios
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('pages > Faq.vue', () => {
-  it('berhasil mounting komponen', () => {
-    const wrapper: any = shallowMount(PowerRank, {
+  beforeEach(() => {
+    // set criteria
+    wrapper = shallowMount(PowerRank, {
       stubs: ['nuxt-link'],
       data() {
         return {
@@ -12,13 +24,72 @@ describe('pages > Faq.vue', () => {
         };
       },
     });
+  });
+
+  it('berhasil mounting komponen', () => {
     expect(wrapper.vm).toBeTruthy();
   });
 
   it('berhasil mounting komponen tanpa token', () => {
-    const wrapper: any = shallowMount(PowerRank, {
+    const mockedResponse: AxiosResponse = {
+      data: {
+        response_code: '0000',
+        message: 'test success',
+        data: [{
+          id: '1',
+          criteria_name: 'string',
+          percent_progress: 'number',
+          slug: 'design',
+          description: 'string',
+          shortdec: 'string',
+        }, {
+          id: '2',
+          criteria_name: 'string',
+          percent_progress: 'number',
+          slug: 'quality',
+          description: 'string',
+          shortdec: 'string',
+        }],
+      },
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    };
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+
+    wrapper = shallowMount(PowerRank, {
       stubs: ['nuxt-link'],
     });
     expect(wrapper.vm).toBeTruthy();
+  });
+
+  it('test comparisonProcess()', () => {
+    const mockedResponse: AxiosResponse = {
+      data: {
+        response_code: '0000',
+        message: 'test success',
+        data: [
+          {
+            score: 0.02291289235442241, employee_id: '60c1c6088b9165fab214c6e4', employee_name: 'Septema Umna Maulida', employee_email: 'septema@alterra.id',
+          },
+          {
+            score: 0.022905904234226733, employee_id: '60c1c6088b9165fab214c6da', employee_name: 'Mas Agus Setyo Purwanto', employee_email: 'agus@alterra.id',
+          },
+          {
+            score: 0.02210301382876708, employee_id: '60c1c6088b9165fab214c6dc', employee_name: 'Herman Sutanto', employee_email: 'herman@alterra.id',
+          },
+          {
+            score: 0.02210301382876708, employee_id: '6103aaf832720826e811358c', employee_name: 'Doni H', employee_email: 'dhadiansyah@alterra.id',
+          },
+        ],
+      },
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+    };
+    mockedAxios.get.mockResolvedValue(mockedResponse);
+    wrapper.vm.comparisonProcess();
   });
 });
