5c026f28f994386f69ac516980ad70f543503a2e
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-02 12:24:41 +0700

    HOTI-1279 unit testing

--- src/tests/pages/powerRank.spec.ts
+++ src/tests/pages/powerRank.spec.ts
@@ -21,6 +21,18 @@ describe('pages > Faq.vue', () => {
       data() {
         return {
           token: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MjMzMTQ2NTEuMTcxMDc0NCwidXNlcl9pZCI6IjYwYjQ2ZGYxZmQ0MGU5Yjk4MjZiZjAzNyIsInVzZXJfb2F1dGhfaWQiOiIxMTMzMzI3NjE3NTgwODA3Njc5MjkiLCJ1c2VyX2VtYWlsIjoiZGhhcm1hd2FuQGFsdGVycmEuaWQiLCJ1c2VyX25hbWUiOiJEaGFybWF3YW4gU3VrbWEgSGFyZGkgUHJhdGFtYSIsInVzZXJfb3JnYW5pemF0aW9uIjoiVEVDIC0gRU5HIiwidXNlcl9vcmdhbml6YXRpb25fZnVsbF90ZXh0IjoiVGVjaG5vbG9neSAtIEVuZ2luZWVyaW5nIiwidXNlcl9idXNpbmVzc191bml0IjoiSE8gLSBUZWNobm9sb2d5In0.Aw8qbhOYxf1E7nrG84n7xAj4i269AJVIW3wocHcB4Vg',
+          comparison: [
+            { score: 0.02291289235442241, employee_email: 'septema@alterra.id' },
+            { score: 0.022905904234226733, employee_email: 'agus@alterra.id' },
+            { score: 0.02210301382876708, employee_email: 'herman@alterra.id' },
+            { score: 0.02210301382876707, employee_email: 'one@alterra.id' },
+            { score: 0.02210301382876706, employee_email: 'two@alterra.id' },
+            { score: 0.02210301382876705, employee_email: 'three@alterra.id' },
+            { score: 0.02210301382876704, employee_email: 'four@alterra.id' },
+            { score: 0.02210301382876703, employee_email: 'five@alterra.id' },
+            { score: 0.02210301382876702, employee_email: 'six@alterra.id' },
+            { score: 0.02210301382876701, employee_email: 'seven@alterra.id' },
+          ],
         };
       },
     });
@@ -91,5 +103,34 @@ describe('pages > Faq.vue', () => {
     };
     mockedAxios.get.mockResolvedValue(mockedResponse);
     wrapper.vm.comparisonProcess();
+  });
+
+  it('test getRanker()', () => {
+    // normal
+    wrapper.vm.getRanker(0);
+    wrapper.setData({ user: 'nouser@alterra.id' });
+
+    // else
+    wrapper.vm.getRanker(0);
+
+    // point 5
+    wrapper.setData({ user: 'agus@alterra.id' });
+    wrapper.vm.getRanker(0);
+
+    // point 4
+    wrapper.setData({ user: 'herman@alterra.id' });
+    wrapper.vm.getRanker(0);
+
+    // point 3
+    wrapper.setData({ user: 'two@alterra.id' });
+    wrapper.vm.getRanker(0);
+
+    // point 2
+    wrapper.setData({ user: 'four@alterra.id' });
+    wrapper.vm.getRanker(0);
+
+    // else
+    wrapper.setData({ user: 'six@alterra.id' });
+    wrapper.vm.getRanker(0);
   });
 });
