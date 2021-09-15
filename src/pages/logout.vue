ea27ec4563b38f452fb56ee8f26212bada30fa2b
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-15 16:55:57 +0700

    HOTI-1357 logout

--- src/pages/logout.vue
+++ src/pages/logout.vue
@@ -7,9 +7,7 @@
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator';
 
-@Component({
-  layout: 'login',
-})
+@Component
 export default class Logout extends Vue {
   mounted() {
     this.init();
