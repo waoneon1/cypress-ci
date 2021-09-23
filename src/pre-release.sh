5b9e64b826808d93c70e8b6996c7717024c05323
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-23 10:19:23 +0700

    HOTI-1446 version updater pint

--- src/pre-release.sh
+++ src/pre-release.sh
@@ -8,12 +8,11 @@ NC='\033[0m' # No Color
 echo -e "Updating release version from ${GREEN}${CURRENT_VERSION}${NC} to ${GREEN}${NEW_VERSION}${NC}"
 
 sed -i '' "s/${CURRENT_VERSION}/${NEW_VERSION}/" package.json
-sed -i '' "s/${CURRENT_VERSION}/${NEW_VERSION}/" package.json
-echo -e "o ${GREEN}package.json updated to ${NEW_VERSION}${NC}"
+echo -e "● ${GREEN}package.json updated to ${NEW_VERSION}${NC}"
 
 SONAR_FILE=$(find ./ -iname sonar*.properties -type f)
 # echo "Sonar file found: ${SONAR_FILE}"
 REPLACE='^sonar.projectVersion=.*$'
 WITH="sonar.projectVersion=${NEW_VERSION}"
 sed -i '' "s#${REPLACE}#${WITH}#g" ${SONAR_FILE}
-echo -e "o ${GREEN}sonar-project.properties updated to ${NEW_VERSION}${NC}"
+echo -e "● ${GREEN}sonar-project.properties updated to ${NEW_VERSION}${NC}"
