703b35933d705d4510595e0409ede4cab2436f7e
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-28 17:06:35 +0700

    testing with env parameter 10

--- src/deploy-env.sh
+++ src/deploy-env.sh
@@ -2,12 +2,9 @@
 
 # Make File
 touch cypress.env.json
-# echo '{
-#   "BASEURAL": "",
-#   "NUXT_ENV_CYPRESS_TOKEN": ""
-# }' >> cypress2.env.json
+${CYPRESS_ENV} >> cypress.env.json
 
 # Fill the .Env File
-sed -i -e 's|CYPRESS_ENV|'"${CYPRESS_ENV}"'|g' cypress.env.json
+# sed -i -e 's|CYPRESS_ENV|'"${CYPRESS_ENV}"'|g' cypress.env.json
 # sed -i -e 's|NUXT_ENV_CYPRESS_TOKEN|'"${NUXT_ENV_CYPRESS_TOKEN}"'|g' cypress2.env.json
 
