25423f1ae33f6ac195c632a5ce6dd4a8f452b0de
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-28 13:41:14 +0700

    testing 2

--- src/cypress/integration/login.spec.js
+++ src/cypress/integration/login.spec.js
@@ -11,6 +11,7 @@ describe('Login Page', {
   });
 
   it('Login page show success', () => {
+    cy.log(Cypress.env('title'))
     const title = 'Know Your Best';
     cy.get('h1').should('include.text', title);
   });
