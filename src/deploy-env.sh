34972520bfeccfb20d7622ba1dc1528c56096c43
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-28 16:29:29 +0700

    testing with env parameter 8

--- src/deploy-env.sh
+++ src/deploy-env.sh
@@ -1,3 +1,6 @@
+# Make File
+touch cypress.env.json
+
 # Fill the .Env File
 sed -i -e 's|BASEURL|'"${BASEURL}"'|g' cypress.env.json
 sed -i -e 's|NUXT_ENV_CYPRESS_TOKEN|'"${NUXT_ENV_CYPRESS_TOKEN}"'|g' cypress.env.json
