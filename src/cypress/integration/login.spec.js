43833d8d81892ebbf06c122b40b27f42d777f475
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-29 10:10:33 +0700

    testing with env parameter 16

--- src/cypress/integration/login.spec.js
+++ src/cypress/integration/login.spec.js
@@ -24,13 +24,16 @@ describe('Login Page', {
     cy.loginByGoogleApi();
 
     cy.visit('/dashboard');
-    const loginData = jwtDecode("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzI5Njk1MDMuMTgxMTA5MiwidXNlcl9pZCI6IjYwYjQ2ZGYxZmQ0MGU5Yjk4MjZiZjAzNyIsInVzZXJfb2F1dGhfaWQiOiIxMTMzMzI3NjE3NTgwODA3Njc5MjkiLCJ1c2VyX2VtYWlsIjoiZGhhcm1hd2FuQGFsdGVycmEuaWQiLCJ1c2VyX25hbWUiOiJEaGFybWF3YW4gU3VrbWEgSGFyZGkgUHJhdGFtYSIsInVzZXJfb3JnYW5pemF0aW9uIjoiVEVDIC0gRU5HIiwidXNlcl9vcmdhbml6YXRpb25fZnVsbF90ZXh0IjoiVGVjaG5vbG9neSAtIEVuZ2luZWVyaW5nIiwidXNlcl9pbWFnZV91cmwiOiJodHRwczovL3RhbGVudGEuczMuYXAtc291dGhlYXN0LTEuYW1hem9uYXdzLmNvbS9hdmF0YXIvVlhlcnZVa0VtOExva0ZtYTdlMTJGMzRjNVRTZXhudU8uanBnIiwidXNlcl9idXNpbmVzc191bml0IjoiSE8gLSBUZWNobm9sb2d5In0.NfQ3ieKpFEDEqIRUVdKN9wrAsOh3hfObDnf1TEKgd5M");
+    cy.log('the log here')
+    cy.log(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'))
+    const loginData = jwtDecode(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'));
     const usernameArray = _.split(loginData.user_name, ' ', 2);
     const username = _.join(usernameArray, ' ');
 
     // dashboard
     cy.log('Dashboard Page');
     cy.get('h1').should('have.text', username);
+    cy.url().should('eq', `${Cypress.env('BASEURL')}/dashboard`);
   });
 
   it('Login Failed', () => {
