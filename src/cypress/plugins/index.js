6a267412877d154cea0bc8f53245add6e401290b
Dharmawan Sukma <waone.on1@gmail.com>
2021-09-29 13:35:28 +0700

    testing with env parameter 24

--- src/cypress/plugins/index.js
+++ src/cypress/plugins/index.js
@@ -1,7 +1,13 @@
 const { startDevServer } = require('@cypress/webpack-dev-server');
 const { getWebpackConfig } = require('nuxt');
+require('dotenv').config();
 
 module.exports = (on, config) => {
+
+  config.env.title = process.env.NUXT_ENV_RRS_APP_TITLE;
+  config.env.NUXT_ENV_CYPRESS_TOKEN = process.env.NUXT_ENV_CYPRESS_TOKEN;
+  config.env.BASEURL = process.env.BASEURL;
+
   on('dev-server:start', async (options) => {
     const webpackConfig = await getWebpackConfig();
 
