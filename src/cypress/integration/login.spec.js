ea0262340c5d545a0a60b726372971daa04cf487
Ekky Patria <ekky@alterra.id>
2021-09-29 10:29:54 +0700

    test

--- src/cypress/integration/login.spec.js
+++ src/cypress/integration/login.spec.js
@@ -2,42 +2,46 @@ import jwtDecode from 'jwt-decode';
 
 const _ = require('lodash');
 
-describe('Login Page', {
-  viewportHeight: 700,
-  viewportWidth: 900,
-}, () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+describe(
+  'Login Page',
+  {
+    viewportHeight: 700,
+    viewportWidth: 900,
+  },
+  () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
 
-  it('Login page show success', () => {
-    const title = 'Know Your Best';
-    cy.get('h1').should('include.text', title);
-  });
+    it('Login page show success', () => {
+      const title = 'Know Your Best';
+      cy.get('h1').should('include.text', title);
+    });
 
-  it('Button clicked Success', () => {
-    cy.get('#googleLoginBtn').should('not.have.class', 'active').click();
-    cy.get('#googleLoginBtn').should('have.class', 'active');
-  });
+    it('Button clicked Success', () => {
+      cy.get('#googleLoginBtn').should('not.have.class', 'active').click();
+      cy.get('#googleLoginBtn').should('have.class', 'active');
+    });
 
-  it('Login Success', () => {
-    cy.loginByGoogleApi();
+    it(`Login Success: ${Cypress.env('NUXT_ENV_CYPRESS_TOKEN')}`, () => {
+      cy.loginByGoogleApi();
 
-    cy.visit('/dashboard');
-    cy.log('the log here')
-    cy.log(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'))
-    const loginData = jwtDecode(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'));
-    const usernameArray = _.split(loginData.user_name, ' ', 2);
-    const username = _.join(usernameArray, ' ');
+      cy.visit('/dashboard');
+      cy.log('the log here');
+      cy.log(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'));
+      const loginData = jwtDecode(Cypress.env('NUXT_ENV_CYPRESS_TOKEN'));
+      const usernameArray = _.split(loginData.user_name, ' ', 2);
+      const username = _.join(usernameArray, ' ');
 
-    // dashboard
-    cy.log('Dashboard Page');
-    cy.get('h1').should('have.text', username);
-    cy.url().should('eq', `${Cypress.env('BASEURL')}/dashboard`);
-  });
+      // dashboard
+      cy.log('Dashboard Page');
+      cy.get('h1').should('have.text', username);
+      cy.url().should('eq', `${Cypress.env('BASEURL')}/dashboard`);
+    });
 
-  it('Login Failed', () => {
-    cy.visit('/dashboard');
-    cy.url().should('include', 'redirect_url');
-  });
-});
+    it('Login Failed', () => {
+      cy.visit('/dashboard');
+      cy.url().should('include', 'redirect_url');
+    });
+  },
+);
